258bd16ebb81568b398c1e2111d59c7672568c3a
Armel <armelfli50@gmail.com>
2019-12-02 12:39:58 -0600

    added script to index.php

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
+
+   <script src=".front_end/javaScript/user.js?v=7"></script>
+   <script src=".front_end/javaScript/spending.js?v=8"></script>
+   <script src=".front_end/javaScript/goal.js?v=4000"></script>
+   <script src=".front_end/javaScript/chart.js?v=39"></script>
+  <script src=".front_end/javaScript/main.js?v=0=1009"> </script>
+ <link rel="stylesheet" href=".front_end/stylesheets/main.css">
 <?php include_once("./front_end/main.html"); ?>
